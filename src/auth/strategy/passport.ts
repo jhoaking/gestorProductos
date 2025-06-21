87159a9db0a5d807c6afba2a39778dd7b44c5279
jhoaking <peredoroj@gmail.com>
2025-06-21 11:09:17 -0400

    passport mas tipado estricto terminados

--- src/auth/strategy/passport.ts
+++ src/auth/strategy/passport.ts
@@ -3,25 +3,65 @@ import {
   GITHUB_CLIENT_SECRET,
   OAUTH_CALLBACK_URL,
 } from "@/config";
-import passport from "passport";
+import passport, { Profile } from "passport";
 import { Strategy as GithubStrategy } from "passport-github2";
+import { UserService } from "../service/oauth2Service";
+import { validateOauth } from "../schema/oaauth2Schema";
+import { Provider } from "../types/typesUser";
 
-passport.serializeUser((user, done) => {
-  done(null, user.provider);
+
+const userClass = new UserService();
+
+passport.serializeUser((user: any, done) => {
+  done(null, { email: user.email, Provider: user.provider });
 });
 
-passport.deserializeUser(async (id, done) => {
-  done(null, id);
-});
+passport.deserializeUser(
+  async (user: { email: string; provider: string }, done) => {
+    const users = await userClass.findUserById(
+      user.email,
+      user.provider as any
+    );
+    done(null, users);
+  }
+);
 
 passport.use(
   new GithubStrategy(
     {
-      clientID: GITHUB_CLIENT_ID,
-      clientSecret: GITHUB_CLIENT_SECRET,
-      callbackURL: OAUTH_CALLBACK_URL,
+      clientID: GITHUB_CLIENT_ID as string,
+      clientSecret: GITHUB_CLIENT_SECRET as string,
+      callbackURL: OAUTH_CALLBACK_URL as string,
       scope: ["user:email"],
     },
-    async (accessToken, refresh_token, profile, done) => {}
+    async (
+      _accessToken: string,
+      _refresh_token: string,
+      profile: Profile,
+      done: (error: any, user?: Express.User | false) => void
+    ) => {
+      try {
+        // creando nuestr propios datos que vienen desde ihtub para tener control y no lleguen datos malos
+        const email = profile.emails?.[0]?.value ?? "";
+        const username = profile.username ?? "github_user";
+        const avatar_url = profile.photos?.[0]?.value ?? "";
+        const provider = profile.provider ?? "github";
+
+        const userRaw = { email, username, avatar_url, provider };
+
+        const userExistente = await userClass.findUserById(email, provider as Provider);
+        if (userExistente) {
+          return done(null, userExistente);
+        }
+
+        const validateUser = validateOauth(userRaw);
+
+        const guardarUser = await userClass.createUser(validateUser);
+
+        done(null, guardarUser);
+      } catch (error) {
+        done(error, false);
+      }
+    }
   )
 );
